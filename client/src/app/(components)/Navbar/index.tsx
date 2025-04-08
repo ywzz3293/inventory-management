b4fa7ba3f02442b9368c382d8763985b9b01f40f
ywzz3293 <50302188+ywzz3293@users.noreply.github.com>
2025-04-08 13:00:59 -0400

    correct error

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -66,13 +66,13 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            {/* <Image
+            { <Image
               src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
               className="rounded-full h-full object-cover"
-            /> */}
+            /> }
             <span className="font-semibold">Ed Roh</span>
           </div>
         </div>
